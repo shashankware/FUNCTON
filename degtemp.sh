eacfdcb0283dbb20262985ee16b4321ed1d7b32e
shashank <shashank.w1998@gmail.com>
2021-02-18 01:16:43 +0530

    palin added

--- degtemp.sh
+++ degtemp.sh
@@ -27,3 +27,22 @@ case $choice in
 esac
 
 
+#!/bin/bash
+sum=0
+pallindrome () {
+read -p "Enter number:" N
+N1=$N
+while [ $N -gt 0 ]
+do
+   temp=$(($N%10))
+   sum=$(($sum*10+$temp))
+   N=$(($N/10))
+done
+if [ $N1 -eq $sum ]
+then
+   echo "its pallindrome"
+else
+   echo "not pallindrome"
+fi
+}
+
